25c7f49189dd8439554cd08cfaca2d416e0b6b9b
meezee91 <muhammadtarmizisulaiman@gmail.com>
2024-02-17 12:47:37 +0800

    trying both CI & CD 3

--- main.tf
+++ main.tf
@@ -7,7 +7,3 @@ resource "aws_sns_topic" "user_updates" {
   name = var.abc
 }
 
-#add this, then move it to environments/dev.tfvars
-abc="dev"
-#add this, then move it to environments/prod.tfvars
-abc="prod"

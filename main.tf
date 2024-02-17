35c31b5f43079571dffb61d7d0dbd1878db22a1e
meezee91 <muhammadtarmizisulaiman@gmail.com>
2024-02-17 12:45:45 +0800

    trying both CI & CD 2

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce5-tfstate-bucket-1"
-    key    = "tarmizi.tfstate"
-    region = "us-east-1"
-  }
+locals {
+  topic-name = "updates-topic"
 }
 
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-#new sns topic 
+#new sns topic
 resource "aws_sns_topic" "user_updates" {
-  name = "tarmizi-updates-topic"
+  name = var.abc
 }
+
+#add this, then move it to environments/dev.tfvars
+abc="dev"
+#add this, then move it to environments/prod.tfvars
+abc="prod"
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable abc {
+  type        = string
+  default     = "tarmizi-updates-topic"
+  description = "this is a type"
+}
